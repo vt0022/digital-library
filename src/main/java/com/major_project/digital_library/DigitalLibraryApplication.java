b387c1b13c44c8f79131b130be16525862571f2d
Thuan Nguyen <vanthuan2004@gmail.com>
2023-08-24 20:39:53 +0700

    Create the project

---
+++ src/main/java/com/major_project/digital_library/DigitalLibraryApplication.java
@@ -0,0 +1,14 @@
+package com.major_project.digital_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DigitalLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/major_project/digital_library/DigitalLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.major_project.digital_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
